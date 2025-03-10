b880fa1172f709b333cdd2b5641011804ee45b58
Akalchi <alejandrafdez1699@gmail.com>
2025-03-10 17:28:42 +0100

    "FEAT [C is G] implemented to fix falinig test"

--- src/main/java/dev/alejandra/RnaTranscription.java
+++ src/main/java/dev/alejandra/RnaTranscription.java
@@ -3,7 +3,12 @@ package dev.alejandra;
 public class RnaTranscription {
 
     public String toRna(String dnaStrand) {
+        if (dnaStrand.isEmpty()) {
+            return "";
+        }
+        if (dnaStrand.equals("C")) {
+            return "G";
+        }
         return "";
     }
-
 }
